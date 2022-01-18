f26428b13b2d87355b7e8e359ae3552d7df13d5f
Chris <chris@MSI>
2022-01-18 15:32:13 +0000

    Aplicacion backend creada

---
+++ servidor/app/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+$sqlStmt = $_POST['query'];
+$tipoQuery = $_GET['tipo'];
+
+$servername = "db";
+$username = "test";
+$password = "test";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$result = $conn->query($sqlStmt);
+$filas = array();
+if ($result->num_rows > 0) {
+  
+  while($row = $result->fetch_assoc()) {
+    $filas[] = $row;
+  }
+}
+print json_encode($filas);
+
+
+?>
