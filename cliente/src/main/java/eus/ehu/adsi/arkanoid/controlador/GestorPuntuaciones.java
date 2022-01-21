9091131601af33bd4654f963ffa883f0289f94be
soyAngela <angelagonzalez1999@hotmail.es>
2022-01-21 20:52:40 +0100

    Ordenar puntuaciones iguales

--- cliente/src/main/java/eus/ehu/adsi/arkanoid/controlador/GestorPuntuaciones.java
+++ cliente/src/main/java/eus/ehu/adsi/arkanoid/controlador/GestorPuntuaciones.java
@@ -25,10 +25,10 @@ public class GestorPuntuaciones {
 		JSONArray ranking = new JSONArray();
 		ResultadoSQL resultado = null;
 		if(dificultad==0){
-			resultado = GestorDB.getGestorDB().execSQL("SELECT NombreUsuario, Tiempo, Numero FROM Puntuacion ORDER BY Numero ASC");
+			resultado = GestorDB.getGestorDB().execSQL("SELECT NombreUsuario, Tiempo, Numero FROM Puntuacion ORDER BY Numero,ValorFechaHora, NombreUsuario DESC");
 		}
 		else{
-			resultado = GestorDB.getGestorDB().execSQL("SELECT nombreUsuario, Tiempo, Numero FROM Puntuacion WHERE idNivel =="+ dificultad +" ORDER BY Numero ASC");
+			resultado = GestorDB.getGestorDB().execSQL("SELECT nombreUsuario, Tiempo, Numero FROM Puntuacion WHERE idNivel =="+ dificultad +" ORDER BY Numero,ValorFechaHora, NombreUsuario DESC");
 		}
 		if (resultado != null){
 			while (resultado.hasNext()){
