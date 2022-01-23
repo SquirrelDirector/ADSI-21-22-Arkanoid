c74e8e20abc7179ed58b084995aeddcba5d204be
Mourouh <Jonmoro2000@gmail.com>
2022-01-23 23:26:05 +0100

    Commit de Naiara revertido

--- cliente/src/main/java/eus/ehu/adsi/arkanoid/controlador/GestorNiveles.java
+++ cliente/src/main/java/eus/ehu/adsi/arkanoid/controlador/GestorNiveles.java
@@ -21,17 +21,7 @@ public class GestorNiveles {
         String preg="SELECT Velocidad, Anchura, Num_Ladrillos FROM Nivel WHERE idNivel ="+idNivel+";";
         ResultadoSQL rs = GestorDB.getGestorDB().execSQL(preg);
         Double DatosNivel[] = new Double[3];
-        double velocidad = 0;
-        int bola = (int)rs.get("Velocidad");
-        if (bola == 1) velocidad = 0.1;
-		else if (bola == 2) velocidad = 0.11;
-		else if (bola == 3) velocidad = 0.12;
-		else if (bola == 4) velocidad = 0.13;
-		else if (bola == 5) velocidad = 0.14;
-		else if (bola == 6) velocidad = 0.15;
-		else if (bola == 7) velocidad = 0.16;
-		else if (bola == 8) velocidad = 0.17;
-        DatosNivel[0]= velocidad;
+        DatosNivel[0]= Double.parseDouble((String) rs.get("Velocidad"));
         DatosNivel[1]= Double.parseDouble((String) rs.get("Anchura"));
         DatosNivel[2]= Double.parseDouble((String) rs.get("Num_Ladrillos"));
         return DatosNivel;
